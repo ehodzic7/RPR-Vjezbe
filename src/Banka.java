26fd34a0f06b1f04e7bd2f97ad7334869bd29dfb
ehodzic7 <148075961+ehodzic7@users.noreply.github.com>
2023-10-25 22:34:10 +0200

    Add files via upload

---
+++ src/Banka.java
@@ -0,0 +1,15 @@
+public class Banka {
+    private long brojRacuna;
+
+    public long getBrojRacuna() {
+        return brojRacuna;
+    }
+
+    public void setBrojRacuna(long brojRacuna) {
+        this.brojRacuna = brojRacuna;
+    }
+
+    public Banka(long brojRacuna) {
+        this.brojRacuna = brojRacuna;
+    }
+}
---
+++ src/Osoba.java
@@ -0,0 +1,32 @@
+public class Osoba {
+    private String ime;
+    private String prezime;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "ime='" + ime + '\'' +
+                ", prezime='" + prezime + '\'' +
+                '}';
+    }
+
+    public Osoba() {
+        super();
+    }
+}
